62f409e5002256fcb16d12ee81c469f22582805f
Parvez Khan <parvezkhan0802@gmail.com>
2023-07-01 23:24:49 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# **ShutterScrape**
+
+ShutterScrape is a web scrapper for bulk downloading images and videos from Shutterstock with speed. ⚡
+It implements Selenium for browser automation and Beautiful Soup for parsing.
+
+# **Setting up**
+
+Configure shutterscrape.py to your Python version.
+
+Install requirements from Terminal:
+
+pip install beautifulsoup4
+pip install selenium
+pip install lxml
+Install ChromeDriver.
+
+(Optional) Configure environment variables paths for python.exe and chromedriver.exe.
+
+# **Running**
+Open terminal in the directory of shutterscrape.py and enter:
+
+python shutterscrape.py
+Go grab a cup of coffee while waiting... oh wait, it's already done!
+
+# **Definitions**
+Search mode: Enter i for scraping images and v for scraping videos .
+Number of search terms: For example, if you want to search for drone single person, enter 3.
+Search term: Keyword(s) for searching on Shutterstock.
+Number of pages to scrape: Higher number of pages means greater quantity of content with lower keyword precision.
+# **Updates**
+10/1/2020
+Updated for new shutterstock page layout as of 10/1/2020.
+
+4/26/2019
+Updated for new shutterstock page layout as of 4/26/2019.
+
+10/1/2018
+Added GUI for save directory selection.
+
+07/31/2018
+More stability fixes.
+
+07/25/2018
+Added gettyscrape.py for scraping videos from Getty Images.
+
+07/23/2018
+Stability fixes.
---
+++ gettyscrape.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import os
+import tkinter as tk
+from tkinter import filedialog
+import time
+
+def videoscrape():
+    try:
+        chromeOptions = webdriver.ChromeOptions()
+        prefs = {"download.default_directory": scrape_directory}
+        chromeOptions.add_experimental_option("prefs", prefs)
+        driver = webdriver.Chrome(options=chromeOptions)
+        driver.maximize_window()
+        container_window_handle = None
+        while not container_window_handle:
+            container_window_handle = driver.current_window_handle
+        for i in range(1, searchPage + 1):
+            url = "https://www.gettyimages.com/videos/" + searchTerm + "?page=" + str(i)
+            driver.get(url)
+            print("Page " + str(i))
+            for j in range(0, 100):
+                while True:
+                    container = driver.find_elements_by_xpath("//article[@gi-asset='" + str(j) + "']")
+                    if len(container) != 0:
+                        break
+                    if len(driver.find_elements_by_xpath("//article[@gi-asset='" + str(j + 1) + "']")) == 0 and i == searchPage:
+                        driver.close()
+                        return
+                    time.sleep(10)
+                    driver.get(url)
+                    print(str(j))
+                section = container[0].find_element_by_xpath(".//section[@class='image-section']")
+                link = section.find_element_by_xpath(".//a[@class='search-result-asset-link']")
+                video_url = link.get_attribute("href")
+                driver.get(video_url)
+                while True:
+                    wait = WebDriverWait(driver, 30).until(ec.visibility_of_element_located((By.XPATH, "//video[@autoplay='true']")))
+                    data = driver.execute_script("return document.documentElement.outerHTML")
+                    scraper = BeautifulSoup(data, "lxml")
+                    video_container = scraper.find_all("video", {"autoplay": "true"})
+                    if len(video_container) != 0:
+                        break
+                    time.sleep(10)
+                    driver.get(video_url)
+                video_src = video_container[0].get("src")
+                name = video_src.rsplit("/", 1)[-1]
+                try:
+                    driver.get(video_src + "?p=1")
+                    print("Scraped " + name)
+                except Exception as e:
+                    print(e)
+                driver.get(url)
+    except Exception as e:
+        print(e)
+
+print("GettyScrape v1.1")
+
+scrape_directory = ""
+
+while True:
+    while True:
+        print("Please select a directory to save your scraped files.")
+        scrape_directory = filedialog.askdirectory()
+        if scrape_directory == "":
+            print("You must select a directory to save your scraped files.")
+            continue
+        break
+    searchCount = int(input("Number of search terms: "))
+    if searchCount < 1:
+        print("You must have at least one search term.")
+        continue
+    searchTerm = input("Enter search term(s) separated by space: ")
+    searchPage = int(input("Number of pages to scrape: "))
+    if searchPage < 1:
+        print("You must scrape at least one page.")
+        continue
+
+    videoscrape()
+
+    print("Scraping complete.")
+    restartScrape = input("Keep scraping? ('y' for yes or 'n' for no) ")
+    if restartScrape.lower() == "n":
+        print("Scraping ended.")
+        break
